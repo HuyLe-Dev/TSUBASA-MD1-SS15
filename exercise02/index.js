f3bd9c383aba013aae19e833f0027377bde27b9b
MinhHuy <dev.minhuy@gmail.com>
2025-11-14 16:54:27 +0900

    update

--- exercise02/index.js
+++ exercise02/index.js
@@ -8,6 +8,12 @@ let stringMock = arr.reverse().join("");
 console.log("arr.reverse().flat()", arr.reverse().join(""));
 
 // Bài 2: Viết một chương trình khai báo một mảng ký tự. In ra số “ký tự số” trong mảng.
+let str0 = ["c", "s", "c", "2", "6", "1"];
+str0.filter(function (item, index) {
+  if (!isNaN(item)) {
+    console.log(`Phan tu thu ${index + 1} la ky tu so. DO LA SO ${item}`);
+  }
+});
 
 // Bài 3: Viết một chương trình để đếm số ký tự trong một chuỗi.
 let a = "Cắt giao diện về Responsive";
@@ -22,5 +28,31 @@ console.log(
 );
 
 // Bài 4: Viết một chương trình nhập vào 2 chuỗi, sau đó kiểm tra xem hai chuỗi có giống nhau hay không.
+let str1 = String(prompt("Nhap chuoi 1: "));
+let str2 = String(prompt("Nhap chuoi 2: "));
 
+if (str1 === str2) {
+  console.log("Hai chuoi bang nhau");
+} else if (str1 > str2) {
+  console.log("Chuoi 1 lon hon!");
+} else if (str1 < str2) {
+  console.log("Chuoi 2 lon hon!");
+}
 // Bài 5: Viết chương trình nhập vào một mảng ký tự, tìm tất cả các ký tự trong mảng là (-) và thay thế các ký tự này bởi ký tự ( _ )
+let arr5 = [];
+let count5 = 1;
+while (count5 <= 5) {
+  let str3 = String(prompt("Nhap chuoi ky tu: "));
+  arr5.push(str3);
+  count5++;
+}
+
+let newArray5 = arr5.map(function (item) {
+  if (item === "-") {
+    return (item = "_");
+  } else {
+    return item;
+  }
+});
+
+console.log("newArray5", newArray5);
